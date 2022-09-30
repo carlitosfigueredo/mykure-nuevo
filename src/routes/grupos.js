202e1776ccef544df0e212c04d79517a96fd5bf3
Carlos Figueredo <carlos@nosotroshq.com>
2022-09-30 17:49:29 -0400

    Motivo: Modulo grupos
    [Autor: Carlos Figueredo AKA: Dramitas]

--- src/routes/grupos.js
+++ src/routes/grupos.js
@@ -5,13 +5,24 @@ const db = require('./../database');
 
 //Metodo listar todos OK
 router.get('/', async(req, res) => {
-    const grupo = await db.query("SELECT * FROM grupo");
-    res.render('grupos/index', { grupo });
+    try {
+        const grupo = await db.query("SELECT * FROM grupo");
+        res.render('grupos/index', { grupo });
+    } catch (error) {
+        console.log(error);
+        next();
+    }
 });
 
-router.get('/todos', async(req, res) => {
-    const grupo = await db.query("SELECT * FROM grupo");
-    res.render('grupos/index', { grupo });
+router.get('/todos', async(req, res, next) => {
+    try {
+        const grupo = await db.query("SELECT * FROM grupo");
+        res.render('grupos/index', { grupo });
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+
 });
 
 //Para agregar
@@ -21,49 +32,78 @@ router.get('/agregar', async(req, res) => {
 
 
 //Metodo Agregar
-router.post('/agregar', async(req, res) => {
+router.post('/agregar', async(req, res, next) => {
     const { nombreGrupo } = req.body;
     const newGrupo = {
         nombreGrupo
     };
-    await db.query("INSERT INTO grupo SET ?", [newGrupo]);
-    req.flash('success', 'El grupo ha sido creado correctamente');
-    res.redirect('/grupos/todos');
+    try {
+        await db.query("INSERT INTO grupo SET ?", [newGrupo]);
+        req.flash('success', 'El grupo ha sido creado correctamente');
+        console.log(db.query);
+        res.redirect('/grupos/todos');
+
+    } catch (error) {
+        req.flash('fail', 'Ha ocurrido un error. Intente nuevamente por favor.');
+        console.log(error);
+        next();
+    }
+
 });
 
 //Metodo Eliminar
 router.get('/eliminar/:id', async(req, res) => {
-    // const { id } = req.params;
-    // await db.query("DELETE FROM lugar WHERE idLugar = ?", [id]);
-    // req.flash('warning', 'El lugar ha sido eliminado correctamente.');
-    // res.redirect('/ubicaciones/todos');
+    const { id } = req.params;
+    try {
+        await db.query("DELETE FROM grupo WHERE idGrupo = ?", [id]);
+        req.flash('warning', 'El lugar ha sido eliminado correctamente.');
+        res.redirect('/grupos/todos');
+    } catch (error) {
+        req.flash('fail', 'Ha ocurrido un error. Intente nuevamente por favor.');
+        console.log(error);
+        next();
+    }
+
 });
 
 //Metodos Editar
 router.get('/editar/:id', async(req, res) => {
-    // const { id } = req.params;
-    // const lugar = await db.query('SELECT * FROM lugar WHERE idLugar =?', [id]);
-    // res.render('ubicaciones/editar', { lugar: lugar[0] }); //para ver un solo objeto
+    const { id } = req.params;
+    const grupo = await db.query('SELECT * FROM grupo WHERE idGrupo =?', [id]);
+    res.render('grupos/editar', { grupo: grupo[0] }); //para ver un solo objeto
 });
 
-router.post('/editar/:id', async(req, res) => {
-    // const { id } = req.params;
-    // const { nombreLugar } = req.body;
-    // const newLugar = {
-    //     nombreLugar
-    // };
-    // await db.query("UPDATE lugar SET ? WHERE idLugar = ?", [newLugar, id]);
-    // req.flash('success', 'El lugar ha sido editado correctamente.');
-    // res.redirect('/ubicaciones/todos');
+router.post('/editar/:id', async(req, res, next) => {
+    const { id } = req.params;
+    const { nombreGrupo } = req.body;
+    const newGrupo = {
+        nombreGrupo
+    };
+    try {
+        await db.query("UPDATE grupo SET ? WHERE idGrupo = ?", [newGrupo, id]);
+        req.flash('success', 'El grupo ha sido editado correctamente.');
+        res.redirect('/grupos/todos');
+
+    } catch (error) {
+        req.flash('fail', 'Ha ocurrido un error. Intente nuevamente por favor.');
+        console.log(error);
+        next();
+    }
 });
 
 // router.get('/ver/:id', async(req, res) => {
-router.get('/ver/:id', async(req, res) => {
+router.get('/ver/:id', async(req, res, next) => {
     const { id } = req.params;
-    const detalleGrupo = await db.query("SELECT nombreCompletoTutor,carreraParticipante,matriculaParticipante,nombreCompletoParticipante  FROM grupo JOIN grupoDetalles ON grupo.idGrupo = grupoDetalles.idGrupo JOIN participante ON participante.idParticipante = grupoDetalles.idParticipante JOIN tutor ON tutor.idTutor = grupoDetalles.idTutor WHERE grupo.idGrupo = ?", [id]);
-    const grupo = await db.query("SELECT nombreGrupo FROM grupo WHERE idGrupo = ? LIMIT 1", [id]);
-    const tutor = await db.query("SELECT nombreCompletoTutor FROM tutor JOIN grupoDetalles ON tutor.idTutor = grupoDetalles.idTutor JOIN grupo ON grupo.idGrupo = grupoDetalles.idGrupo WHERE grupo.idGrupo = ? LIMIT 1", [id]);
-    res.render('grupos/ver', { detalleGrupo, grupo, tutor });
+    try {
+        const detalleGrupo = await db.query("SELECT nombreCompletoTutor,carreraParticipante,matriculaParticipante,nombreCompletoParticipante  FROM grupo JOIN grupoDetalles ON grupo.idGrupo = grupoDetalles.idGrupo JOIN participante ON participante.idParticipante = grupoDetalles.idParticipante JOIN tutor ON tutor.idTutor = grupoDetalles.idTutor WHERE grupo.idGrupo = ?", [id]);
+        const grupo = await db.query("SELECT nombreGrupo FROM grupo WHERE idGrupo = ? LIMIT 1", [id]);
+        const tutor = await db.query("SELECT nombreCompletoTutor FROM tutor JOIN grupoDetalles ON tutor.idTutor = grupoDetalles.idTutor JOIN grupo ON grupo.idGrupo = grupoDetalles.idGrupo WHERE grupo.idGrupo = ? LIMIT 1", [id]);
+        res.render('grupos/ver', { detalleGrupo, grupo, tutor });
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+
 });
 
 
