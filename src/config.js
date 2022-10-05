2066eb90b63ed1d08db2db30d7868dc6698ecbba
carlitosfigueredo <carlosalberto.figueredoquevedo@gmail.com>
2022-10-05 00:40:21 -0300

    correcciones alan wtf`

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    database: {
-        host: 'sv2.prodominios.com',
-        user: 'unida_dev',
-        password: 'UdaelwYkOkM5TSq',
-        database: 'unida_evento3'
-    }
     // database: {
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: 'HolaManola$$2020',
+    //     host: 'sv2.prodominios.com',
+    //     user: 'unida_dev',
+    //     password: 'UdaelwYkOkM5TSq',
     //     database: 'unida_evento3'
     // }
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'HolaManola$$2020',
+        database: 'unida_evento3'
+    }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('./../database');
 
 
-router.get('/', (req, res) => {
-    res.render('index');
+
+router.get('/', async(req, res) => {
+    try {
+        const ganancias = await db.query('SELECT COUNT(idPulsera) * 3000 as gananciaPulsera FROM pulsera WHERE estadoPulsera = "entregado"');
+        const pulseras = await db.query('SELECT COUNT(idPulsera) as entregadas FROM pulsera WHERE estadoPulsera = "entregado"');
+        const personasRegistradas = await db.query('SELECT COUNT(idPersona) as registradas FROM persona');
+
+        res.render('index', { ganancias: ganancias[0], pulseras: pulseras[0], personasRegistradas: personasRegistradas[0] });
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 module.exports = router;
