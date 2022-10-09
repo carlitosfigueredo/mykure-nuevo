5038db1b8ef7424703017734d0cc86d6d5bc4411
carlitosfigueredo <carlosalberto.figueredoquevedo@gmail.com>
2022-10-09 15:06:20 -0300

    Eventos - mejorar menu

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,15 +10,14 @@ router.get('/', async(req, res, next) => {
         const pulseras = await db.query('SELECT COUNT(idPulsera) as entregadas FROM pulsera WHERE estadoPulsera = "entregado"');
         const personasRegistradas = await db.query('SELECT COUNT(idPersona) as registradas FROM persona');
         const eventos = await db.query('SELECT * FROM evento where estadoEvento = "en proceso" ORDER BY idEvento ASC LIMIT 5');
+        const eventosSecundarios = await db.query('SELECT * FROM eventoSecundario LIMIT 5');
         const cuentaGrupos = await db.query('SELECT COUNT(idGrupo) as cuentaGrupos FROM grupo');
         const cuentaParticipantes = await db.query('SELECT COUNT(idParticipante) as cuentaParticipante from participante');
-        console.log(cuentaGrupos[0]);
-        console.log(cuentaParticipantes[0]);
-
-        res.render('index', { ganancias: ganancias[0], pulseras: pulseras[0], personasRegistradas: personasRegistradas[0], eventos, cuentaGrupos: cuentaGrupos[0], cuentaParticipantes: cuentaParticipantes[0] });
+        res.render('index', { ganancias: ganancias[0], pulseras: pulseras[0], personasRegistradas: personasRegistradas[0], eventos, cuentaGrupos: cuentaGrupos[0], cuentaParticipantes: cuentaParticipantes[0], eventosSecundarios });
     } catch (err) {
         console.log(err);
         req.flash('fail', 'Error' + err.code);
+        res.redirect('/');
         next();
     }
 })
