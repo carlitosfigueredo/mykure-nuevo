00c1b1501e4bd994376244ad1f1e83b1591d99b3
carlitosfigueredo <carlosalberto.figueredoquevedo@gmail.com>
2022-10-09 01:24:05 -0300

    Pulseras

--- src/routes/pulseras.js
+++ src/routes/pulseras.js
@@ -3,14 +3,31 @@ const conexion = require('./../database');
 const router = express.Router();
 const db = require('./../database');
 
-router.get('/', async(req, res) => {
-    const pulsera = await db.query("SELECT idPulsera,codigoPulsera,estadoPulsera,PulseraGanador FROM pulsera");
-    res.render('pulseras/index', { pulsera });
+router.get('/', async(req, res, next) => {
+    try {
+        const pulsera = await db.query("SELECT idPulsera,codigoPulsera,estadoPulsera,PulseraGanador FROM pulsera");
+        res.render('pulseras/index', { pulsera });
+    } catch (err) {
+        console.log(err);
+        req.flash('fail', err.code);
+        res.redirect('/pulseras/todos');
+        next();
+    }
+
 });
 
-router.get('/todos', async(req, res) => {
-    const pulsera = await db.query("SELECT idPulsera,codigoPulsera,estadoPulsera,PulseraGanador FROM pulsera");
-    res.render('pulseras/index', { pulsera });
+router.get('/todos', async(req, res, next) => {
+    try {
+        const pulsera = await db.query("SELECT idPulsera,codigoPulsera,estadoPulsera,PulseraGanador FROM pulsera");
+        res.render('pulseras/index', { pulsera });
+    } catch (err) {
+        console.log(err);
+        req.flash('fail', err.code);
+        res.redirect('/pulseras/todos');
+
+        next();
+    }
+
 });
 
 //Para agregar
@@ -18,45 +35,68 @@ router.get('/agregar', async(req, res) => {
     res.render('pulseras/agregar');
 });
 
-//Metodo Agregar //Metodo listar todos OK idPulsera	codigoPulsera	estadoPulsera	PulseraGanador
-router.post('/agregar', async(req, res) => {
-    const { codigoPulsera, estadoPulsera, PulseraGanador } = req.body;
-    const newTutor = {
-        codigoPulsera,
-        estadoPulsera,
-        PulseraGanador
-    };
-    await db.query("INSERT INTO pulsera SET ?", [newTutor]);
-    req.flash('success', 'Pulsera agregada correctamente.');
-    res.redirect('/pulseras/todos');
+//Metodo Agregar //Metodo listar todos OK idPulsera     codigoPulsera   estadoPulsera   PulseraGanador
+router.post('/agregar', async(req, res, next) => {
+    try {
+        const { codigoPulsera, estadoPulsera, PulseraGanador } = req.body;
+        const newPulsera = {
+            codigoPulsera,
+            estadoPulsera,
+            PulseraGanador
+        };
+        await db.query("INSERT INTO pulsera SET ?", [newPulsera]);
+        req.flash('success', 'Pulsera agregada correctamente.');
+        res.redirect('/pulseras/todos');
+    } catch (err) {
+        console.log(err);
+        req.flash('fail', 'Error. ' + err.code + ' - No se pueden repetir los codigos de pulseras');
+        res.redirect('/pulseras/agregar');
+        next();
+    }
+
 });
 
 //Metodo Eliminar
-router.get('/eliminar/:id', async(req, res) => {
-    // const { id } = req.params;
-    // await db.query("DELETE FROM lugar WHERE idLugar = ?", [id]);
-    // req.flash('warning', 'El lugar ha sido eliminado correctamente.');
-    // res.redirect('/ubicaciones/todos');
+router.get('/eliminar/:id', async(req, res, next) => {
+    const { id } = req.params;
+    try {
+        await db.query("DELETE FROM pulsera WHERE idPulsera = ?", [id]);
+        req.flash('warning', 'La pulsera ha sido eliminado correctamente.');
+        res.redirect('/pulseras/todos');
+    } catch (err) {
+        req.flash('fail', 'No se puede eliminar' + err.code);
+        res.redirect('/pulseras/todos');
+        next();
+    }
+
 });
 
 //Metodos Editar
 router.get('/editar/:id', async(req, res) => {
-    // const { id } = req.params;
-    // const lugar = await db.query('SELECT * FROM lugar WHERE idLugar =?', [id]);
-    // res.render('ubicaciones/editar', { lugar: lugar[0] }); //para ver un solo objeto
+    const { id } = req.params;
+    const pulsera = await db.query('SELECT * FROM pulsera WHERE idpulsera =?', [id]);
+    res.render('pulseras/editar', { pulsera: pulsera[0] }); //para ver un solo objeto
 });
 
-router.post('/editar/:id', async(req, res) => {
-    // const { id } = req.params;
-    // const { nombreLugar } = req.body;
-    // const newLugar = {
-    //     nombreLugar
-    // };
-    // await db.query("UPDATE lugar SET ? WHERE idLugar = ?", [newLugar, id]);
-    // req.flash('success', 'El lugar ha sido editado correctamente.');
-    // res.redirect('/ubicaciones/todos');
+router.post('/editar/:id', async(req, res, next) => {
+    const { id } = req.params;
+    const { codigoPulsera, estadoPulsera, PulseraGanador } = req.body;
+    const newPulsera = {
+        codigoPulsera,
+        estadoPulsera,
+        PulseraGanador
+    };
+    try {
+        await db.query("UPDATE pulsera SET ? WHERE idPulsera = ?", [newPulsera, id]);
+        req.flash('warning', 'Pulsera editada correctamente.');
+        res.redirect('/pulseras/todos');
+    } catch (err) {
+        console.log(err);
+        req.flash('fail', 'Error. ' + err.code + 'No se pueden repetir los codigos de pulseras');
+        res.redirect('/pulseras/todos');
+        next();
+    }
 });
 
 
 //Exportar modulos
-module.exports = router;
